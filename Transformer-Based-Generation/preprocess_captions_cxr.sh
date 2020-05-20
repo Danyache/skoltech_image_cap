105d91c18015b3df73c2f8fe78b7f6822d5a4168
Daniil Chesakov <dchesakov@nes.ru>
2020-05-20 19:14:35 +0300

    transf test

---
+++ Transformer-Based-Generation/preprocess_captions_cxr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+export PYTHONPATH=.:./external/coco-caption
+
+preprocess/preprocess_captions_cxr.sh --ms-coco-dir $1 --split train
+preprocess/preprocess_captions_cxr.sh --ms-coco-dir $1 --split valid
+preprocess/preprocess_captions_cxr.sh --ms-coco-dir $1 --split test
+
+# Separately tokenize captions for self-critical sequence training
+# python preprocess/tokenize_captions_scst.py --ms-coco-dir $1
